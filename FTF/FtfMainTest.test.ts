677f2c84415c3b23d9616cc9ccef5e87831a00f7
Stephanie CP <stephers.cp@gmail.com>
2023-05-03 17:38:06 -0700

    Added step to return to home page after test 2.0 to avoid 3.0 to fail

--- FTF/FtfMainTest.test.ts
+++ FTF/FtfMainTest.test.ts
@@ -22,15 +22,15 @@ describe("1- Basic reality check", () => {
 describe("2- Can search based on address", () => {
     test("2.0 - ___", async() => {
         await ftfMainPageObject.searchWithAddressFromHome('104 S WC Riles Dr, Flagstaff, AZ 86001')
+        await ftfMainPageObject.navigate() // Returns home
     }) 
 }) // Verfied and passing - 4/29/23
 
 // Commenting becuse imcomplete method leads to test file failing
-describe.only("3- Fill form to stay up to date (no submiting) - Clicks / Form entry", () => {
+describe("3- Fill form to stay up to date (no submiting) - Clicks / Form entry", () => {
     test("3.0 - Navigate to a fill form to stay up to date (email signup)", async() => {
         console.log ("(3) Start")
         await ftfMainPageObject.navigateToStayUpToDateForm()
-
         await ftfMainPageObject.hardCodedStayUpToDateFormFilling('John', 'Doe', 'johndoe@test.com')
         console.log ("(3) Done")
     }) 
